96bc16f00e188de27e6cf990f080e18eba9c1a20
Renan Claude <renanclaude.rcg@gmail.com>
2023-03-03 17:15:18 -0300

    Requisitos 2, 3, 4, 5, e 6 - feitos (faltando testes em 4 e 6)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Login } from './Login';
+import { Switch, Route } from 'react-router-dom';
+import { Login } from './page/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <Login />
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/meals" component={ Recipes } />
+    </Switch>
   );
 }
 

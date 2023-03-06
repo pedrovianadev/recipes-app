b36ad8ffe852dd4b45e15cbacf75dc1a1c9687c4
Leonardo Anzulin <leoanzulin@gmail.com>
2023-03-06 15:00:15 -0300

    Implementando footer nas rotas

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile() {
   return (
@@ -8,6 +9,7 @@ function Profile() {
         title="Profile"
         haveSearch={ false }
       />
+      <Footer />
     </div>
   );
 }

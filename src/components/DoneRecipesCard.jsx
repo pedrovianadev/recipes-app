e71d84eccd86ead20300d3e7ee46f84dee963be1
Leonardo Anzulin <leoanzulin@gmail.com>
2023-03-09 11:28:22 -0300

    Requisito 45

--- src/components/DoneRecipesCard.jsx
+++ src/components/DoneRecipesCard.jsx
@@ -1,18 +1,25 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import logo from '../images/shareIcon.svg';
 
 function DoneRecipesCard({ index }) {
   const tagName1 = 'Pasta';
   const tagName2 = 'Curry';
   return (
     <div>
-      <img alt="foodpicture" data-testid={ `${index}-horizontal-image` } />
-      <h3 data-testid={ `${index}-horizontal-name` }>Titulo</h3>
-      <p data-testid={ `${index}-horizontal-top-text` }>Categoria</p>
-      <p data-testid={ `${index}-horizontal-done-date` }>Data de Conclusão</p>
-      <button data-testid={ `${index}-horizontal-share-btn` }>compartilhar</button>
-      <p data-testid={ `${index}-${tagName1}-horizontal-tag` }>Tag</p>
-      <p data-testid={ `${index}-${tagName2}-horizontal-tag` }>Tag</p>
+      <img alt="foodpicture" src="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg" data-testid={ `${index}-horizontal-image` } />
+      <h3 data-testid={ `${index}-horizontal-name` }>Spicy Arrabiata Penne</h3>
+      <p data-testid={ `${index}-horizontal-top-text` }>Italian - Vegetarian</p>
+      <p data-testid={ `${index}-horizontal-done-date` }>23/06/2020</p>
+      <button
+        data-testid={ `${index}-horizontal-share-btn` }
+        src={ logo }
+      >
+        compartilhar
+
+      </button>
+      <p data-testid={ `${index}-${tagName1}-horizontal-tag` }>{tagName1}</p>
+      <p data-testid={ `${index}-${tagName2}-horizontal-tag` }>{tagName2}</p>
     </div>
   );
 }

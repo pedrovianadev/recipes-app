e93e15ccb5a3921f85851185c22f10b8514d62e1
Leonardo Anzulin <leoanzulin@gmail.com>
2023-03-09 19:16:16 -0300

    Requisito 50 e 51

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
+import FavoriteRecipesCard from '../components/FavoriteRecipesCard';
+import FilterDonePag from '../components/FilterDonePag';
 
 function FavoriteRecipes() {
+  const array = [0, 1, 2];
   return (
     <div>
       <Header
         title="Favorite Recipes"
         haveSearch={ false }
       />
+      <FilterDonePag />
+      {array.map((number) => <FavoriteRecipesCard index={ number } key={ number } />)}
     </div>
   );
 }

124152e58ccb061f99674ab0ba82d22099c35f80
Leonardo Anzulin <leoanzulin@gmail.com>
2023-03-10 15:45:04 -0300

    Requisito 55

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -2,28 +2,67 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import FavoriteMealCard from '../components/FavoriteMealCard';
 import FavoriteDrinkCard from '../components/FavoriteDrinkCard';
-import FilterDonePag from '../components/FilterDonePag';
 import { getFavoriteRecipes } from '../services/saveFavoriteRecipes';
 
 function FavoriteRecipes() {
   const [array, setArray] = useState([]);
-  const showRecipes = async () => {
-    const musics = await getFavoriteRecipes();
-    setArray(musics);
-  };
+  const [filter, setFilter] = useState({
+    meal: false,
+    drink: false,
+    all: true,
+  });
+
   useEffect(
     () => {
+      const showRecipes = async () => {
+        const recipes = await getFavoriteRecipes();
+        if (filter.all === true) {
+          setArray(recipes);
+        } else if (filter.meal === true) {
+          const filterMeal = recipes.filter((recipe) => recipe.type === 'meal');
+          setArray(filterMeal);
+        } else if (filter.drink === true) {
+          const filterDrink = recipes.filter((recipe) => recipe.type === 'drink');
+          setArray(filterDrink);
+        }
+      };
       showRecipes();
     },
-    [array],
+    [array, filter],
   );
+  const handleMeal = () => {
+    setFilter(
+      { meal: true,
+        drink: false,
+        all: false,
+      },
+    );
+  };
+  const handleDrink = () => {
+    setFilter(
+      { meal: false,
+        drink: true,
+        all: false,
+      },
+    );
+  };
+  const handleAll = () => {
+    setFilter(
+      { meal: false,
+        drink: false,
+        all: true,
+      },
+    );
+  };
   return (
     <div>
       <Header
         title="Favorite Recipes"
         haveSearch={ false }
       />
-      <FilterDonePag />
+      <button data-testid="filter-by-all-btn" onClick={ handleAll }>All</button>
+      <button data-testid="filter-by-meal-btn" onClick={ handleMeal }>Meals</button>
+      <button data-testid="filter-by-drink-btn" onClick={ handleDrink }>Drinks</button>
       {array.map((recipes, index) => (
         recipes.type === 'meal'
           ? (

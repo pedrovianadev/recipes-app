a4a3efd41c4ce229ea8e692029a47daa856574aa
Pedro Viana <pedroviana.developer13@gmail.com>
2023-03-13 18:32:30 -0300

    .

--- src/pages/RecipesInProgress.jsx
+++ src/pages/RecipesInProgress.jsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { getMealsByID, getDrinksByID } from '../services/api';
 import IngredientsCards from '../components/IngredientsCards';
 
+const START_INDEX = -1;
+let index = START_INDEX;
+
 function RecipesInProgress() {
   const [recipe, setRecipe] = useState([]);
   const [typeOfRecipe, setTypeOfRecipe] = useState('');
   const { pathname } = window.location;
   const id = pathname.split('/')[2];
+  let inProgressRecipes = JSON
+    .parse(localStorage.getItem('inProgressRecipes'))
+    || { drinks: { 178319: [] }, meals: { 52771: [] } };
 
   useEffect(() => {
     const kindOfRecipe = pathname.split('/')[1];
@@ -15,6 +21,7 @@ function RecipesInProgress() {
     if (kindOfRecipe === 'meals') {
       const getMeals = async () => {
         const response = await getMealsByID(id);
+        console.log(response, 'linha 21');
         setRecipe(response.meals[0]);
       };
       getMeals();
@@ -25,7 +32,13 @@ function RecipesInProgress() {
       };
       getDrinks();
     }
+    if (!inProgressRecipes[kindOfRecipe]) {
+      inProgressRecipes = { ...inProgressRecipes, [kindOfRecipe]: { [id]: [] } };
+      localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
+    }
   }, [typeOfRecipe]);
+
+  console.log(recipe);
 
   return (
     <div>
@@ -57,8 +70,9 @@ function RecipesInProgress() {
           <p data-testid="instructions">{recipe.strInstructions}</p>
           <h2>Ingredientes</h2>
           <ul>
-            {Object.keys(recipe).map((key, index) => {
+            {Object.keys(recipe).map((key) => {
               if (key.includes('Ingredient') && recipe[key]) {
+                index += 1;
                 return (
                   <li key={ index }>
                     <IngredientsCards
